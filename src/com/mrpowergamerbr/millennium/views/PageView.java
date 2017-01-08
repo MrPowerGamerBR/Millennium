1e81cc84e1658e90daaf4e09f5c931a5f1cb92f5
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-01-08 19:36:13 -0200

    Alterações no PostView/PageView
    Agora o PageView simplesmente redireciona para o PostView para
    compartilhar mais códigos.
    
    Corrigido alguns exceptions em vez de dar erro 404 no PostView.

--- src/com/mrpowergamerbr/millennium/views/PageView.java
+++ src/com/mrpowergamerbr/millennium/views/PageView.java
@@ -1,55 +1,10 @@
 package com.mrpowergamerbr.millennium.views;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.HashMap;
-import org.bson.Document;
 import org.jooby.Request;
 import org.jooby.Response;
 
-import com.mitchellbosecke.pebble.error.PebbleException;
-import com.mitchellbosecke.pebble.template.PebbleTemplate;
-import com.mongodb.client.model.Filters;
-import com.mrpowergamerbr.millennium.Millennium;
-import com.mrpowergamerbr.millennium.utils.RenderWrapper;
-import com.mrpowergamerbr.millennium.utils.blog.Page;
-import com.mrpowergamerbr.millennium.utils.blog.Post;
-
 public class PageView {
-
 	public static Object render(Request req, Response res) {
-		try {
-			HashMap<String, Object> context = new HashMap<String, Object>();
-
-			String[] args = req.path().split("/");
-			// args[0] = ""
-			// args[1] = "posts"
-			// args[2] = "slug"
-
-			String slug = args[2];
-
-			Document doc = Millennium.client.getDatabase("millennium").getCollection("pages").find(Filters.eq("slug", slug)).first();
-
-			if (doc != null) {
-				Post post = Millennium.datastore.get(Page.class, doc.get("_id"));
-
-				context.put("post", Millennium.fillPost(post));
-
-				post.addOneMoreView(req);
-			} else {
-				return Error404View.render(req, res);
-			}
-
-			PebbleTemplate template = Millennium.engine.getTemplate("post.html");
-
-			return new RenderWrapper(template, context);
-		} catch (PebbleException e) {
-			// TODO Auto-generated catch block
-			StringWriter sw = new StringWriter();
-			PrintWriter pw = new PrintWriter(sw);
-			e.printStackTrace(pw);
-			return e.toString();
-		}
-
+		return PostView.render(req, res, true);
 	}
 }
--- src/com/mrpowergamerbr/millennium/views/PostView.java
+++ src/com/mrpowergamerbr/millennium/views/PostView.java
@@ -3,8 +3,6 @@ package com.mrpowergamerbr.millennium.views;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
-
 import org.bson.Document;
 import org.jooby.Request;
 import org.jooby.Response;
@@ -14,37 +12,40 @@ import com.mitchellbosecke.pebble.template.PebbleTemplate;
 import com.mongodb.client.model.Filters;
 import com.mrpowergamerbr.millennium.Millennium;
 import com.mrpowergamerbr.millennium.utils.RenderWrapper;
-import com.mrpowergamerbr.millennium.utils.StrUtils;
 import com.mrpowergamerbr.millennium.utils.blog.Post;
 
 public class PostView {
-
 	public static Object render(Request req, Response res) {
+		return render(req, res, false);
+	}
+
+	public static Object render(Request req, Response res, boolean isPage) {
 		try {
 			HashMap<String, Object> context = new HashMap<String, Object>();
 
 			String[] args = req.path().split("/");
-			// args[0] = ""
-			// args[1] = "posts"
-			// args[2] = "slug"
 
-			String slug = args[2];
+			if (args.length >= 3) {
+				String slug = args[2];
 
-			Document doc = Millennium.client.getDatabase("millennium").getCollection("posts").find(Filters.eq("slug", slug)).first();
+				Document doc = Millennium.client.getDatabase("millennium").getCollection(isPage ? "pages" : "posts").find(Filters.eq("slug", slug)).first();
 
-			if (doc != null) {
-				Post post = Millennium.datastore.get(Post.class, doc.get("_id"));
+				if (doc != null) {
+					Post post = Millennium.datastore.get(Post.class, doc.get("_id"));
 
-				context.put("post", Millennium.fillPost(post));
+					context.put("post", Millennium.fillPost(post));
 
-				post.addOneMoreView(req);
+					post.addOneMoreView(req);
+				} else {
+					return Error404View.render(req, res);
+				}
+
+				PebbleTemplate template = Millennium.engine.getTemplate("post.html");
+
+				return new RenderWrapper(template, context);
 			} else {
 				return Error404View.render(req, res);
 			}
-
-			PebbleTemplate template = Millennium.engine.getTemplate("post.html");
-
-			return new RenderWrapper(template, context);
 		} catch (PebbleException e) {
 			// TODO Auto-generated catch block
 			StringWriter sw = new StringWriter();
@@ -52,6 +53,5 @@ public class PostView {
 			e.printStackTrace(pw);
 			return e.toString();
 		}
-
 	}
 }
